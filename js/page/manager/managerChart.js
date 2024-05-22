fc20ecf72ad76361dc4f43282e74f416aa37565d
manhthanh201 <nguyenmanhthanh2001>
2024-05-22 14:55:44 +0700

    .

--- js/page/manager/managerChart.js
+++ js/page/manager/managerChart.js
@@ -1,134 +1,160 @@
 myMap = null;
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAKWLInk3HEpmsL438LX41LXuVYROs_DYE",
+  authDomain: "pandemic-management-1f2fd.firebaseapp.com",
+  projectId: "pandemic-management-1f2fd",
+  storageBucket: "pandemic-management-1f2fd.appspot.com",
+  messagingSenderId: "60561382808",
+  appId: "1:60561382808:web:f3977c82b16d6310ca5fed",
+  measurementId: "G-TR2HC0H8SP",
+};
+firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore();
+
 window.onload = () => {
-    unitApi = new UnitApi();
-    myMap = new MyMap();
-}
+  unitApi = new UnitApi();
+  myMap = new MyMap();
+};
 
 class MyMap extends Base {
-    constructor() {
-        super();
-        this.initEvent();
-        this.loadHeaderInfo();
-        this.showChart();
+  constructor() {
+    super();
+    this.initEvent();
+    this.loadHeaderInfo();
+    this.getUnitData();
+  }
+
+  initEvent() {}
+
+  getUnitData() {
+    const urlParams = new URLSearchParams(window.location.search);
+    let unitCode = urlParams.get("unitCode");
+    if (unitCode) {
+      unitApi.getSingleUnitInfo(unitCode).then((res) => {
+        this.loadChartData(unitCode, res.data.unitName);
+      });
+    } else {
+      this.loadChartData("|", "Cả nước");
     }
+  }
 
-    initEvent() {
+  loadChartData(unitCode, unitName) {
+    db.collection(unitCode)
+      .get()
+      .then((querySnapshot) => {
+        var docsData = [];
+        querySnapshot.forEach((doc) => {
+          docsData.push(doc.data());
+        });
+        return docsData;
+      })
+      .then((objectsArray) => {
+        const resultMap = new Map();
+        objectsArray.forEach((obj) => {
+          const date = obj.date;
+          if (resultMap.has(date)) {
+            const existingObj = resultMap.get(date);
+            existingObj.lastUpdateDeaths += obj.lastUpdateDeaths;
+            existingObj.lastUpdateRecovereds += obj.lastUpdateRecovereds;
+            existingObj.lastUpdateCases += obj.lastUpdateCases;
+          } else {
+            resultMap.set(date, { ...obj });
+          }
+        });
+        const newArray = Array.from(resultMap.values());
+        newArray.sort((a, b) => a.date - b.date);
+        this.showChart(newArray, unitName);
+      });
+  }
 
+  showChart(arr, unitName) {
+    let arrNew = arr.map((e) => {
+      return {
+        x: new Date(e.date),
+        y: e.lastUpdateCases,
+      };
+    });
+
+    let arrRecovered = arr.map((e) => {
+      return {
+        x: new Date(e.date),
+        y: e.lastUpdateRecovereds,
+      };
+    });
+
+    let arrDeath = arr.map((e) => {
+      return {
+        x: new Date(e.date),
+        y: e.lastUpdateDeaths,
+      };
+    });
+
+    var options = {
+      animationEnabled: true,
+      theme: "light2",
+      title: {
+        text: "Tình hình dịch bệnh theo thời gian của " + unitName,
+      },
+      axisX: {
+        valueFormatString: "DD MMM",
+      },
+      axisY: {
+        title: "Số lượng",
+        suffix: "",
+        minimum: 0,
+      },
+      toolTip: {
+        shared: true,
+      },
+      legend: {
+        cursor: "pointer",
+        verticalAlign: "bottom",
+        horizontalAlign: "left",
+        dockInsidePlotArea: true,
+        itemclick: toogleDataSeries,
+      },
+      data: [
+        {
+          type: "line",
+          showInLegend: true,
+          name: "Số ca mắc mới",
+          markerType: "square",
+          xValueFormatString: "DD MMM, YYYY",
+          color: "#FFFF00",
+          yValueFormatString: "#,##0 ca",
+          dataPoints: arrNew,
+        },
+        {
+          type: "line",
+          showInLegend: true,
+          name: "Số ca tử vong",
+          lineDashType: "dash",
+          color: "#FF0000",
+          yValueFormatString: "#,##0 ca",
+          dataPoints: arrDeath,
+        },
+        {
+          type: "line",
+          showInLegend: true,
+          name: "Số ca hồi phục",
+          markerType: "triangle",
+          lineDashType: "dashDot",
+          color: "#008000",
+          yValueFormatString: "#,##0 ca",
+          dataPoints: arrRecovered,
+        },
+      ],
+    };
+    $("#chartContainer").CanvasJSChart(options);
+
+    function toogleDataSeries(e) {
+      if (typeof e.dataSeries.visible === "undefined" || e.dataSeries.visible) {
+        e.dataSeries.visible = false;
+      } else {
+        e.dataSeries.visible = true;
+      }
+      e.chart.render();
     }
-
-    showChart() {
-        var options = {
-            animationEnabled: true,
-            theme: "light2",
-            title: {
-                text: "Tình hình dịch bệnh tại đơn vị theo thời gian"
-            },
-            axisX: {
-                valueFormatString: "DD MMM"
-            },
-            axisY: {
-                title: "Số lượng",
-                suffix: "",
-                minimum: 30
-            },
-            toolTip: {
-                shared: true
-            },
-            legend: {
-                cursor: "pointer",
-                verticalAlign: "bottom",
-                horizontalAlign: "left",
-                dockInsidePlotArea: true,
-                itemclick: toogleDataSeries
-            },
-            data: [
-                {
-                    type: "line",
-                    showInLegend: true,
-                    name: "Số ca mắc mới",
-                    markerType: "square",
-                    xValueFormatString: "DD MMM, YYYY",
-                    color: "#F08080",
-                    yValueFormatString: "#,##0 ca",
-                    dataPoints: [
-                        { x: new Date(2017, 10, 1), y: 63 },
-                        { x: new Date(2017, 10, 2), y: 69 },
-                        { x: new Date(2017, 10, 3), y: 65 },
-                        { x: new Date(2017, 10, 4), y: 70 },
-                        { x: new Date(2017, 10, 5), y: 71 },
-                        { x: new Date(2017, 10, 6), y: 65 },
-                        { x: new Date(2017, 10, 7), y: 73 },
-                        { x: new Date(2017, 10, 8), y: 96 },
-                        { x: new Date(2017, 10, 9), y: 84 },
-                        { x: new Date(2017, 10, 10), y: 85 },
-                        { x: new Date(2017, 10, 11), y: 86 },
-                        { x: new Date(2017, 10, 12), y: 94 },
-                        { x: new Date(2017, 10, 13), y: 97 },
-                        { x: new Date(2017, 10, 14), y: 86 },
-                        { x: new Date(2017, 10, 15), y: 89 }
-                    ]
-                },
-                {
-                    type: "line",
-                    showInLegend: true,
-                    name: "Số ca tử vong",
-                    lineDashType: "dash",
-                    yValueFormatString: "#,##0 ca",
-                    dataPoints: [
-                        { x: new Date(2017, 10, 1), y: 60 },
-                        { x: new Date(2017, 10, 2), y: 57 },
-                        { x: new Date(2017, 10, 3), y: 51 },
-                        { x: new Date(2017, 10, 4), y: 56 },
-                        { x: new Date(2017, 10, 5), y: 54 },
-                        { x: new Date(2017, 10, 6), y: 55 },
-                        { x: new Date(2017, 10, 7), y: 54 },
-                        { x: new Date(2017, 10, 8), y: 69 },
-                        { x: new Date(2017, 10, 9), y: 65 },
-                        { x: new Date(2017, 10, 10), y: 66 },
-                        { x: new Date(2017, 10, 11), y: 63 },
-                        { x: new Date(2017, 10, 12), y: 67 },
-                        { x: new Date(2017, 10, 13), y: 66 },
-                        { x: new Date(2017, 10, 14), y: 56 },
-                        { x: new Date(2017, 10, 15), y: 64 }
-                    ]
-                },
-                {
-                    type: "line",
-                    showInLegend: true,
-                    name: "Số ca hồi phục",
-                    lineDashType: "dash",
-                    yValueFormatString: "#,##0 ca",
-                    dataPoints: [
-                        { x: new Date(2017, 10, 1), y: 60 + 3 },
-                        { x: new Date(2017, 10, 2), y: 57 + 3 },
-                        { x: new Date(2017, 10, 3), y: 51 + 3 },
-                        { x: new Date(2017, 10, 4), y: 56 + 3 },
-                        { x: new Date(2017, 10, 5), y: 54 + 3 },
-                        { x: new Date(2017, 10, 6), y: 55 + 3 },
-                        { x: new Date(2017, 10, 7), y: 54 + 3 },
-                        { x: new Date(2017, 10, 8), y: 69 + 3 },
-                        { x: new Date(2017, 10, 9), y: 65 + 3 },
-                        { x: new Date(2017, 10, 10), y: 66 + 3 },
-                        { x: new Date(2017, 10, 11), y: 63 + 3 },
-                        { x: new Date(2017, 10, 12), y: 67 + 3 },
-                        { x: new Date(2017, 10, 13), y: 66 + 3 },
-                        { x: new Date(2017, 10, 14), y: 56 + 3 },
-                        { x: new Date(2017, 10, 15), y: 64 + 3 }
-                    ]
-                }]
-        };
-        $("#chartContainer").CanvasJSChart(options);
-    
-        function toogleDataSeries(e) {
-            if (typeof (e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
-                e.dataSeries.visible = false;
-            } else {
-                e.dataSeries.visible = true;
-            }
-            e.chart.render();
-        }
-    }
-
-
+  }
 }
--- js/page/manager/managerEpidemicSituation.js
+++ js/page/manager/managerEpidemicSituation.js
@@ -1,164 +1,203 @@
 managerEpidemicSituation = null;
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAKWLInk3HEpmsL438LX41LXuVYROs_DYE",
-    authDomain: "pandemic-management-1f2fd.firebaseapp.com",
-    projectId: "pandemic-management-1f2fd",
-    storageBucket: "pandemic-management-1f2fd.appspot.com",
-    messagingSenderId: "60561382808",
-    appId: "1:60561382808:web:f3977c82b16d6310ca5fed",
-    measurementId: "G-TR2HC0H8SP"
+  apiKey: "AIzaSyAKWLInk3HEpmsL438LX41LXuVYROs_DYE",
+  authDomain: "pandemic-management-1f2fd.firebaseapp.com",
+  projectId: "pandemic-management-1f2fd",
+  storageBucket: "pandemic-management-1f2fd.appspot.com",
+  messagingSenderId: "60561382808",
+  appId: "1:60561382808:web:f3977c82b16d6310ca5fed",
+  measurementId: "G-TR2HC0H8SP",
 };
 
 firebase.initializeApp(firebaseConfig);
 var db = firebase.firestore();
 
-db.collection("your-collection-name").get().then((querySnapshot) => {
+db.collection("|1|276|9997|")
+  .get()
+  .then((querySnapshot) => {
     var docsData = [];
     querySnapshot.forEach((doc) => {
-        docsData.push(doc.data());
+      docsData.push(doc.data());
     });
     return docsData;
-}).then(res => {
-    console.log(res);
-})
+  })
+  .then((res) => {
+    console.log(JSON.stringify(res, null, 2));
+  });
 
 window.onload = () => {
-    unitApi = new UnitApi();
-    managerEpidemicSituation = new ManagerEpidemicSituation();
-}
-
+  unitApi = new UnitApi();
+  managerEpidemicSituation = new ManagerEpidemicSituation();
+};
 
 class ManagerEpidemicSituation extends Base {
-    constructor() {
-        super();
-        this.level = 0;
-        this.unitCode = '|';
-        this.initEvent();
-        this.loadHeaderInfo();
-        this.prepareUnitPage();
+  constructor() {
+    super();
+    this.level = 0;
+    this.unitCode = "|";
+    this.initEvent();
+    this.loadHeaderInfo();
+    this.prepareUnitPage();
+    this.loadListUnit(this.unitCode);
+  }
+
+  prepareUnitPage() {
+    var userUnitCode = sessionStorage.getItem("userUnitCode");
+    if (userUnitCode.split("|").length == 5) {
+      document.querySelector("#btnUpdateUnit").classList.remove("d-none");
+      document.querySelector("#valueUnitDetail").value =
+        sessionStorage.getItem("unitDetail");
+    }
+  }
+
+  initEvent() {
+    document.querySelector("#btnUpdateUnit").addEventListener("click", () => {
+      document.querySelector(".dialog").classList.add("d-block");
+    });
+    document.querySelector(".close-from").addEventListener("click", () => {
+      document.querySelector(".dialog").classList.remove("d-block");
+    });
+    document.querySelector(".search-box").addEventListener("keypress", (e) => {
+      if (e.key === "Enter") {
         this.loadListUnit(this.unitCode);
-    }
+      }
+    });
+    document.querySelector("#btnRefresh").addEventListener("click", () => {
+      document.querySelector(".search-box").value = "";
+      this.index = 0;
+      this.unitCode = "|";
+      this.loadListUnit(this.unitCode);
+    });
+    document.querySelector("#btnReturn").addEventListener("click", () => {
+      document.querySelector(".search-box").value = "";
+      this.index = 0;
+      this.setToParentUnit();
+      this.loadListUnit(this.unitCode);
+    });
+    document
+      .querySelector("#btnConfirmUpdate")
+      .addEventListener("click", () => {
+        let input1 = document.querySelector("#lastUpdateCases");
+        let input2 = document.querySelector("#lastUpdateDeaths");
+        let input3 = document.querySelector("#lastUpdateRecovereds");
+        let input4 = document.querySelector("#updateDateInput");
+        if (input1.value < 0) {
+          showToastMessenger("danger", "Không nhận dữ liệu âm!");
+          input1.focus();
+        } else if (input2.value < 0) {
+          showToastMessenger("danger", "Không nhận dữ liệu âm!");
+          input2.focus();
+        } else if (input3.value < 0) {
+          showToastMessenger("danger", "Không nhận dữ liệu âm!");
+          input3.focus();
+        } else {
+          let body = {
+            lastUpdateCases: Number(input1.value),
+            lastUpdateDeaths: Number(input2.value),
+            lastUpdateRecovereds: Number(input3.value),
+          };
+          let today = new Date();
+          if (input4.value) {
+            today = new Date(input4.value);
+          }
+          today.setHours(0, 0, 0, 0);
+          let number = Number(today);
 
-    prepareUnitPage() {
-        var userUnitCode = sessionStorage.getItem('userUnitCode');
-        if (userUnitCode.split('|').length == 5) {
-            document.querySelector('#btnUpdateUnit').classList.remove('d-none');
-            document.querySelector('#valueUnitDetail').value = sessionStorage.getItem('unitDetail');
+          let wardCode = sessionStorage.getItem("userUnitCode");
+          let arr = wardCode.split("|");
+          let a = arr[1];
+          let b = arr[2];
+          var districtCode = `|${a}|${b}|`;
+          var provinceCode = `|${a}|`;
+
+          db.collection(wardCode).add({ ...body, date: number });
+          db.collection(districtCode).add({ ...body, date: number });
+          db.collection(provinceCode).add({ ...body, date: number });
+          db.collection('|').add({ ...body, date: number });
+
+          unitApi
+            .update(body, wardCode)
+            .then((res) => {
+              console.log(res);
+              showToastMessenger("success", "Cập nhật thành công!");
+              this.loadListUnit(this.unitCode);
+              document.querySelector(".dialog").classList.remove("d-block");
+            })
+            .catch((error) => {
+              console.log(error);
+              showToastMessenger(
+                "danger",
+                "Cập nhật thất bại. Vui lòng thử lại sau!"
+              );
+              document.querySelector(".dialog").classList.remove("d-block");
+            });
         }
-    }
+      });
+  }
 
-    initEvent() {
-        document.querySelector('#btnUpdateUnit').addEventListener('click', () => {
-            document.querySelector('.dialog').classList.add('d-block');
-        })
-        document.querySelector('.close-from').addEventListener('click', () => {
-            document.querySelector('.dialog').classList.remove('d-block');
-        })
-        document.querySelector(".search-box").addEventListener("keypress", (e) => {
-            if (e.key === 'Enter') {
-                this.loadListUnit(this.unitCode);
-            }
-        });
-        document.querySelector('#btnRefresh').addEventListener('click', () => {
-            document.querySelector(".search-box").value = '';
-            this.index = 0;
-            this.unitCode = '|';
-            this.loadListUnit(this.unitCode);
-        });
-        document.querySelector('#btnReturn').addEventListener('click', () => {
-            document.querySelector(".search-box").value = '';
-            this.index = 0;
-            this.setToParentUnit();
-            this.loadListUnit(this.unitCode);
-        })
-        document.querySelector('#btnConfirmUpdate').addEventListener('click', () => {
-            let input1 = document.querySelector('#lastUpdateCases');
-            let input2 = document.querySelector('#lastUpdateDeaths');
-            let input3 = document.querySelector('#lastUpdateRecovereds');
-            if (input1.value < 0) {
-                showToastMessenger('danger', "Không nhận dữ liệu âm!");
-                input1.focus();
-            } else if (input2.value < 0) {
-                showToastMessenger('danger', "Không nhận dữ liệu âm!");
-                input2.focus();
-            } else if (input3.value < 0) {
-                showToastMessenger('danger', "Không nhận dữ liệu âm!");
-                input3.focus();
-            } else {
-                let body = {
-                    lastUpdateCases: Number(input1.value),
-                    lastUpdateDeaths: Number(input2.value),
-                    lastUpdateRecovereds: Number(input3.value)
-                };
-                let today = new Date();
-                today.setHours(0, 0, 0, 0);
+  loadListUnit(unitCode) {
+    showLoader();
+    var keyword = document.querySelector(".search-box").value;
+    unitApi
+      .getById(unitCode, this.getPage(), this.count, keyword)
+      .then((res) => {
+        console.log(res.data);
+        this.total = res.data.length;
+        loadTable(listColums.Unit, res.data, this.index + 1);
+        this.reloadPagingInfo();
+        this.initEventTable();
+      })
+      .catch((error) => {
+        console.log(error);
+        hideLoader();
+        if (error.status == 405) {
+          if (this.index == 0) {
+            loadTable(listColums.Account, [], this.index + 1);
+            showToastMessenger("success", "Không có bản ghi nào cả hiuhiu!");
+          } else {
+            showToastMessenger("danger", "Bạn đã đến trang cuối mất rồi!");
+            this.index = this.index - this.count;
+          }
+        } else {
+          showToastMessenger("danger", "Đã có lỗi, vui lòng thử lại sau!");
+        }
+      });
+  }
 
-                db.collection("your-collection-name").add({ ...body, date: today});
+  getPage() {
+    return (this.index + this.count) / this.count;
+  }
 
-                unitApi.update(body, sessionStorage.getItem('userUnitCode')).then(res => {
-                    console.log(res);
-                    showToastMessenger('success', "Cập nhật thành công!");
-                    this.loadListUnit(this.unitCode);
-                    document.querySelector('.dialog').classList.remove('d-block');
-                }).catch(error => {
-                    console.log(error);
-                    showToastMessenger('danger', "Cập nhật thất bại. Vui lòng thử lại sau!");
-                    document.querySelector('.dialog').classList.remove('d-block');
-                })
-            }
-        })
-    }
-
-    loadListUnit(unitCode) {
-        showLoader();
-        var keyword = document.querySelector('.search-box').value;
-        unitApi.getById(unitCode, this.getPage(), this.count, keyword).then(res => {
-            console.log(res.data);
-            this.total = res.data.length;
-            loadTable(listColums.Unit, res.data, this.index + 1);
-            this.reloadPagingInfo();
-            this.initEventTable();
-        }).catch(error => {
-            console.log(error);
-            hideLoader();
-            if (error.status == 405) {
-                if (this.index == 0) {
-                    loadTable(listColums.Account, [], this.index + 1);
-                    showToastMessenger('success', "Không có bản ghi nào cả hiuhiu!");
-                } else {
-                    showToastMessenger('danger', "Bạn đã đến trang cuối mất rồi!");
-                    this.index = this.index - this.count;
-                }
-            } else {
-                showToastMessenger('danger', "Đã có lỗi, vui lòng thử lại sau!");
-            }
-        })
-    }
-
-    getPage() {
-        return (this.index + this.count) / this.count;
-    }
-
-    tableRowOnDBClick(item, thisTr) {
-        var popupBtns = [{ text: "Đóng", enable: true }, { text: "Xem", enable: true }, { text: "QR Code", enable: true }]
-        var btns = showPopupDialog("Thông báo", "Bạn có muốn xem tình hình dịch các đơn vị con của đơn vị " + item.unitName + " không?", popupBtns);
-        btns[0].addEventListener('click', () => {
-            hidePopupDialog();
-        });
-        btns[1].addEventListener('click', () => {
-            hidePopupDialog();
-            if (item.type == "w") {
-                showToastMessenger("danger", "Đây đã là đơn vị cấp nhỏ nhất!!");
-            } else {
-                this.unitCode = item.unitCode;
-                document.querySelector(".search-box").value = '';
-                this.index = 0;
-                this.loadListUnit(this.unitCode);
-            }
-        });
-        btns[2].addEventListener('click', () => {
-            showUnitQr(item.unitCode);
-        });
-    }
+  tableRowOnDBClick(item, thisTr) {
+    var popupBtns = [
+      { text: "Đóng", enable: true },
+      { text: "Xem", enable: true },
+      { text: "QR Code", enable: true },
+    ];
+    var btns = showPopupDialog(
+      "Thông báo",
+      "Bạn có muốn xem tình hình dịch các đơn vị con của đơn vị " +
+        item.unitName +
+        " không?",
+      popupBtns
+    );
+    btns[0].addEventListener("click", () => {
+      hidePopupDialog();
+    });
+    btns[1].addEventListener("click", () => {
+      hidePopupDialog();
+      if (item.type == "w") {
+        showToastMessenger("danger", "Đây đã là đơn vị cấp nhỏ nhất!!");
+      } else {
+        this.unitCode = item.unitCode;
+        document.querySelector(".search-box").value = "";
+        this.index = 0;
+        this.loadListUnit(this.unitCode);
+      }
+    });
+    btns[2].addEventListener("click", () => {
+      showUnitQr(item.unitCode);
+    });
+  }
 }
